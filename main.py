9707efdd0e406fb80861f5b7cbb05f8cd38314ee
michalskibinski109 <michalskibinski109@gmail.com>
2023-01-06 14:21:56 +0100

    finished project

--- main.py
+++ main.py
@@ -1,57 +1,47 @@
 import os
-from flask import (
-    Flask,
-    flash,
-    request,
-    redirect,
-    render_template,
-    url_for,
-    send_from_directory,
-)
+from app import app
+from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+from prepare_video import ModelRunner
 from pathlib import Path
-from prepare_video import run
+from miskibin import get_logger
 
-uploads = Path(__file__).parent / "videos"
-UPLOAD_FOLDER = uploads.resolve()
-ALLOWED_EXTENSIONS = {"mp4", "avi", "mov", "mkv"}
-
-
-app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+logger = get_logger(lvl=20)
+runner = ModelRunner()
 
 
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route("/")
+def upload_form():
+    return render_template("upload.html")
 
 
-@app.route("/", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        # check if the post request has the file part
-        if "file" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files["file"]
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            # here replace orginal vide with the new one
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            filepath = Path(UPLOAD_FOLDER) / filename
-            run(filepath.resolve())  # here goes all magic
-            return redirect(url_for("download_file", name=filename))
-    return render_template("index.html", uploaded_videos=os.listdir(UPLOAD_FOLDER))
+@app.route("/", methods=["POST"])
+def upload_video():
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    if file.filename == "":
+        flash("No image selected for uploading")
+        return redirect(request.url)
+    else:
+        filename = file.filename
+        logger.info(f"File {filename} is selected for uploading")
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        runner.run(Path(os.path.join(app.config["UPLOAD_FOLDER"], filename)))
+        logger.info(f"File {filename} successfully uploaded")
+        # print('upload_video filename: ' + filename)
+        flash("Video successfully uploaded and displayed below")
+        return render_template(
+            "upload.html", filename=str(filename).replace(".avi", ".mp4")
+        )
 
 
-@app.route("/uploads/<name>")
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route("/display/<filename>")
+def display_video(filename):
+    # print('display_video filename: ' + filename)
+    return redirect(url_for("static", filename="uploads/" + filename), code=301)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- prepare_video.py
+++ prepare_video.py
@@ -1,86 +1,114 @@
 import cv2
-import os
 from pathlib import Path
 from tqdm import tqdm
 from tensorflow.keras.models import load_model
 import numpy as np
 from miskibin import get_logger
 
-logger = get_logger(lvl=10)
 
+class ModelRunner:
+    def __init__(
+        self,
+        logger=get_logger(lvl=10),
+        model_path: Path = Path("model.h5"),
+        model_input_shape=(32, 32),
+    ):
+        self.model = load_model(str(model_path.resolve()))
+        self.logger = logger
+        self.model_input_shape = model_input_shape
 
-def split_video(video_path):
+    def run(self, video_path: Path, fps: int = 30) -> str:
+        """Run model on video and save result to mp4 file.
+        args:
+            `video_path`: path to video file. Also used as output file name.
+            `fps`: frames per second for output video
+        returns:
+            `str`: path to output file
+        """
+        images = self.__split_video(video_path)
+        images = self.__run_model(images)
+        self.__merge_images(images, str(video_path).replace(".avi", ".mp4"), fps)
+        return str(video_path).replace(".avi", ".mp4")
 
-    vidcap = cv2.VideoCapture(str(video_path.resolve()))
-    success, image = vidcap.read()
-    count = 0
-    bar = tqdm(total=vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-    images = []
-    while success:
-        # cv2.imwrite(f"frame{count}.jpg", image)  # save frame as JPEG file
+    def __split_video(self, video_path):
+        vidcap = cv2.VideoCapture(str(video_path.resolve()))
         success, image = vidcap.read()
-        images.append(np.array(image))
-        count += 1
-        bar.update(1)
-    bar.close()
-    logger.info(f"Read {count} frames each of shape {images[0].shape}")
-    return np.array(images)
-
-
-def merge_images(images, path="project.mp4", fps=30):
-    height, width, layers = images[0].shape
-    video = cv2.VideoWriter(path, 0, fps, (width, height))
-    bar = tqdm(total=len(images))
-    for image in images:
-        video.write(image)
-        bar.update(1)
-    bar.close()
-    cv2.destroyAllWindows()
-    video.release()
-
-
-def prepare_images(images, size=(32, 32)):
-    # here goes your code. Images must be prepared for model: resize, grayscale, etc.
-    for i, image in enumerate(images):
-        # image = cv2.resize(image, (32, 32))
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # image = np.expand_dims(image, axis=0)
-        images[i] = np.array(image)
-    return np.array(images)
-
-
-def run_model(images: np.ndarray):
-    # here goes your code. Images must be processed by model. every image must be replaced with processed image
-    model = load_model("model.h5")
-    for i, image in enumerate(images):
-        image = cv2.resize(image, (32, 32))
-
-        # expand dimensions so that it represents a single 'sample'
-        image = np.expand_dims(image, axis=0)
-        # resize image
-        predicted = model.predict(image)
-        bbox = predicted[1][0] * 255
-        print(bbox[0], bbox[1], bbox[2], bbox[3])
-        # draw bounding box on the image
-        cv2.rectangle(
-            image,
-            (int(bbox[0]), int(bbox[1])),
-            (int(bbox[2]), int(bbox[3])),
-            (0, 255, 0),
-            2,
+        count = 0
+        bar = tqdm(total=vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+        images = []
+        while success:
+            # cv2.imwrite(f"frame{count}.jpg", image)  # save frame as JPEG file
+            success, image = vidcap.read()
+            if image is not None:
+                images.append(np.array(image))
+                count += 1
+            bar.update(1)
+        bar.close()
+        self.logger.info(f"Read {count} frames each of shape {images[0].shape}")
+        arr = np.zeros(
+            (count, images[0].shape[0], images[0].shape[1], images[0].shape[2]),
+            dtype=np.uint8,
         )
-        images[i] = image
+        for i, image in enumerate(images):
+            if image is None:
+                break
+            arr[i] = image
+        return arr
 
-        logger.debug(bbox)
-    return images
+    def __prepare_images(
+        self,
+        old_images,
+    ):
+        # here goes your code. Images must be prepared for model: resize, grayscale, etc.
+        new_images = []
+        grey = (100, 100, 100)
+        light_grey = (146, 145, 149)
+        for i, img in enumerate(old_images):
+            mask = cv2.inRange(img, grey, light_grey)
+            result = cv2.bitwise_and(img, img, mask=mask)
+            # grey scale
+            result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+            result = result / 255
+            result = cv2.resize(result, self.model_input_shape)
+            new_images.append(np.array(result))
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # image = np.expand_dims(image, axis=0)
+        return np.array(new_images)
 
+    def __run_model(self, images: np.ndarray):
+        # here goes your code. Images must be processed by model. every image must be replaced with processed image
+        new_images = self.__prepare_images(images)
+        IMG_SIZE = images[0].shape[0]
+        bar = tqdm(total=len(images))
+        for i, image in enumerate(new_images):
+            predicted = self.model.predict(np.expand_dims(image, axis=0), verbose=0)
+            bbox = predicted[1][0]
+            x, y, w, h = bbox * IMG_SIZE
+            start_point = (int(x), int(y))
+            end_point = (int(x + w), int(y + h))
+            try:
+                image = cv2.rectangle(images[i], start_point, end_point, (255, 0, 0), 3)
+            except ValueError as exc:
+                self.logger.debug(f"ValueError {exc}")
+            images[i] = image
+            bar.update(1)
+            self.logger.debug(bbox)
+        bar.close()
+        return images
 
-def run(video_path):
-    images = split_video(video_path)
-    images = prepare_images(images)
-    images = run_model(images)
-    merge_images(images, video_path)
-
-
-if __name__ == "__main__":
-    run(Path("videos") / "f.mp4")
+    def __merge_images(self, images, path, fps=10):
+        height, width = images[0].shape[0], images[0].shape[1]
+        self.logger.warning(
+            f"Video will be saved to {path} with {fps} fps and {width}x{height} size total {images[0].shape}"
+        )
+        fourcc = cv2.VideoWriter_fourcc("a", "v", "c", "1")
+        # video in grey scale
+        video = cv2.VideoWriter(path, fourcc, fps, (width, height), True)
+        bar = tqdm(total=len(images))
+        for image in images:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            video.write(image)
+            bar.update(1)
+        bar.close()
+        cv2.destroyAllWindows()
+        video.release()
